d340db065338af5608c87182e1421df5c4059892
jx12345 <jim@formatc.net>
2016-07-09 10:01:22 +0100

    create app page

--- app.php
+++ app.php
@@ -1,5 +1,13 @@
 <?php 
 session_start();
+require_once __DIR__ . '/vendor/autoload.php';
+
+$fb = new Facebook\Facebook([
+    'app_id' => '1763077917273267',
+    'app_secret' => '02ded92ceb16f6e7b5281cd41b2bb2f3',
+    'default_graph_version' => 'v2.5',
+]);
+
 $accessToken = $_SESSION['facebook_access_token'];
 
 $fb->setDefaultAccessToken($accessToken);
