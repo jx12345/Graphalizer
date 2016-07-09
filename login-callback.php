773a336e70202f05bba544ddc39b8ce027c1f238
jx12345 <jim@formatc.net>
2016-07-09 09:27:43 +0100

    login

--- login-callback.php
+++ login-callback.php
@@ -27,7 +27,7 @@ try {
 if (isset($accessToken)) {
   // Logged in!
   $_SESSION['facebook_access_token'] = (string) $accessToken;
-
+  print_r($_SESSION);
   // Now you can redirect to another page and use the
   // access token from $_SESSION['facebook_access_token']
 }
